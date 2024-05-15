19029afa64a7d188f7d8970ff4ef461993b73b39
Josh Pollock <jmpollock50@gmail.com>
2024-05-15 11:12:41 -0400

    api change

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:3000",
+        target: "https://fakebook-server-omega.vercel.app",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
