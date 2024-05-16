e954e16e796b2029983c2b50f6a8a7534d14802c
Josh Pollock <jmpollock50@gmail.com>
2024-05-16 17:55:48 -0400

    change pool

--- server/database/db_connection.js
+++ server/database/db_connection.js
@@ -8,10 +8,10 @@ dotenv.config();
 
 
 const pool = mysql.createPool({ // collection of connections
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  host: 'fakebook-db.cpm6s2sguyrk.us-east-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'j4PTu6taJN',
+  database: 'fakebook-rds'
 }).promise();      
 
 
