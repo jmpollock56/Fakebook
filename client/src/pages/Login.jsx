677ba96ab4ff0694b55e9b1f780daa0989517913
Josh Pollock <jmpollock50@gmail.com>
2024-05-20 11:42:49 -0400

    updated fetch address

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
     e.preventDefault();
     
     try{
-      const response = await fetch('/api/login', {
+      const response = await fetch('https://fakebook-server-omega.vercel.app/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ emailOrPhone, password })
