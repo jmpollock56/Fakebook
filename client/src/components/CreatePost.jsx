8005efa3e3a1ba00da414234f99d8c026f3fcb50
Josh Pollock <jmpollock50@gmail.com>
2024-05-20 16:56:18 -0400

    pfp changes

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -7,7 +7,7 @@ export default function CreatePost({ handlePostCreation, user}){
   return (
     <div className="flex flex-col h-20 justify-center bg-sky-300 box-content p-6 rounded-md shadow-md"> 
       <div className="flex flex-row items-center justify-evenly w-full mb-5">
-        <img className="size-8 rounded-full " src={user.pfp}/>
+        <img className="size-8 rounded-full " src={(user.pfp) ? user.pfp : "/profile_pictures/default_pfp.png"}/>
         <input className="w-[90%] rounded-full box-content p-2" type="text" placeholder={`What's on your mind, ${user.first_name}?`} onClick={handlePostCreation}/>
       </div>
       
