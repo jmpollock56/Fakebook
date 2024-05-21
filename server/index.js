be3860ed5b4b3bb28ce60e4eeb849248df867597
Josh Pollock <jmpollock50@gmail.com>
2024-05-21 17:36:57 -0400

    req.params

--- server/index.js
+++ server/index.js
@@ -269,7 +269,7 @@ app.post('/api/posts/comment/create', async (req, res) => {
 })
 
 app.get('/api/posts/comments/:post_id', async (req, res) => {
-  const { post_id } = req.params;
+  const post_id  = req.params;
   console.log(post_id);
   try{
     const comments = await getPostComments(post_id);
