e4406c5f2f9dbe19f776f07108d2ef855d9430e6
Josh Pollock <jmpollock50@gmail.com>
2024-05-22 12:10:45 -0400

    css

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -38,33 +38,33 @@ export default function NavBar() {
 
 
   return (
-   <header className="bg-white w-full fixed box-content p-2 z-10 shadow-xl">
-    <nav className="flex justify-between items-center w-[98%] mx-auto">
+   <header className="bg-white h-10 w-full fixed box-content p-2 z-10 shadow-xl">
+    <nav className="flex justify-between items-center w-[98%] h-[100%] mx-auto">
 
       <div className="flex items-center justify-evenly">
-        <img onClick={() => {navigateTo('/home')}}  className="size-8 cursor-pointer mr-2" src="profile_pictures/fb_logo.png" alt="..." />
+        <img onClick={() => {navigateTo('/home')}}  className="size-10 cursor-pointer mr-2" src="profile_pictures/fb_logo.png" alt="..." />
         <input className="rounded-full p-1 bg-[#dfe3ee] pl-5" type="text" placeholder="Search Fakebook"/>
       </div>
 
       <div className="flex items-center h-full w-[50%] justify-center">
 
-        <div onClick={() => {navigateTo('/home')}} className="flex justify-center h-8 w-28 items-center hover:bg-[#dfe3ee] rounded-md cursor-pointer">
-          <CiHome className="size-5"/> 
+        <div onClick={() => {navigateTo('/home')}} className="flex justify-center h-9 w-28 items-center hover:bg-[#dfe3ee] rounded-md cursor-pointer">
+          <CiHome className="size-6"/> 
         </div>
 
-        <div className="flex justify-center items-center h-8 w-28 hover:bg-[#dfe3ee] rounded-md cursor-pointer">
-          <FaUserFriends className="size-5"/>
+        <div className="flex justify-center items-center h-9 w-28 hover:bg-[#dfe3ee] rounded-md cursor-pointer">
+          <FaUserFriends className="size-6"/>
         </div>
 
-        <div className="flex justify-center items-center  h-8 w-28 hover:bg-[#dfe3ee] rounded-md cursor-pointer">
-          <MdOndemandVideo className="size-5"/>
+        <div className="flex justify-center items-center  h-9 w-28 hover:bg-[#dfe3ee] rounded-md cursor-pointer">
+          <MdOndemandVideo className="size-6"/>
         </div>
 
-        <div className="flex justify-center items-center  h-8 w-28 hover:bg-[#dfe3ee] rounded-md cursor-pointer">
-          <CiShop className="size-5"/>
+        <div className="flex justify-center items-center  h-9 w-28 hover:bg-[#dfe3ee] rounded-md cursor-pointer">
+          <CiShop className="size-6"/>
         </div>
 
-        <div className="flex justify-center items-center  h-8 w-28 hover:bg-[#dfe3ee] rounded-md cursor-pointer">
+        <div className="flex justify-center items-center  h-9 w-28 hover:bg-[#dfe3ee] rounded-md cursor-pointer">
          <GrGroup className="size-5"/>
         </div>
             
@@ -72,21 +72,21 @@ export default function NavBar() {
 
       <div className="flex items-center"> 
 
-        <div className="flex items-center justify-center size-8 rounded-full bg-[#dfe3ee] hover:bg-[#cdd5ec] cursor-pointer">
-          <CgMenuGridO className="size-5"/>
+        <div className="flex items-center justify-center size-10 rounded-full bg-[#dfe3ee] hover:bg-[#cdd5ec] cursor-pointer">
+          <CgMenuGridO className="size-7"/>
         </div>
 
-        <div className="flex items-center justify-center size-8 rounded-full bg-[#dfe3ee] ml-2 hover:bg-[#cdd5ec] cursor-pointer">
-          <BiMessageRoundedDetail className="size-5"/>
+        <div className="flex items-center justify-center size-10 rounded-full bg-[#dfe3ee] ml-2 hover:bg-[#cdd5ec] cursor-pointer">
+          <BiMessageRoundedDetail className="size-7"/>
         </div>
 
-        <div className="flex items-center justify-center size-8 rounded-full bg-[#dfe3ee] ml-2 hover:bg-[#cdd5ec] cursor-pointer">
-          <FaRegBell className="w-5  h-5"/>
+        <div className="flex items-center justify-center size-10 rounded-full bg-[#dfe3ee] ml-2 hover:bg-[#cdd5ec] cursor-pointer">
+          <FaRegBell className="size-7"/>
         </div>
         
 
         <div className="relative ml-2 cursor-pointer" onClick={() => {setIsDropDownMenu(!isDropDownMenu)}}>
-          <img className="size-8 rounded-full" src={(currentUser.pfp) ? currentUser.pfp : "/profile_pictures/default_pfp.png"} alt="profile" />
+          <img className="size-10 rounded-full" src={(currentUser.pfp) ? currentUser.pfp : "/profile_pictures/default_pfp.png"} alt="profile" />
           <div className="absolute bottom-0 right-0 rounded-full bg-white">
             <IoIosArrowDown className="h-3 w-3"/>
           </div>
